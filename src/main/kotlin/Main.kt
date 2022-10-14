86fbd78c2275d4818b78a7ed1d98838a2d270d61
tianaustimenko <106236014+tianaustimenko@users.noreply.github.com>
2022-10-14 13:29:59 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,63 @@
+fun main(args: Array<String>) {
+    val exercises:Int=13
+    var exercisesSolved:Int=0
+
+    var myAge: Int=18.00
+
+    var age:Double=(myAge+30)/2
+    print (age)
+
+    val testNumber: Int=17
+    val evenOdd: Int=testNumber%2
+    print(evenOdd)
+
+    var answer=((0+1)+10)*10
+    answer shr 3
+    print(answer)
+
+    var age=16
+    print(age)
+    var age=30
+    print(age)
+
+    val a: Int=46
+    val b: Int=10
+    val answer1 : Int=(a*100)+b
+    print (answer1)
+    val answer2 : Int=(a*100)+(b*100)
+    print (answer2)
+    val answer3 : Int=(a*100)+(b/10)
+    print(answer3)
+
+    print((5*3)-((4/2)*2))
+
+    val a1: Double=7.00
+    val b1: Double=11.00
+    val average: Double=(a1+b1)/2
+    print(average)
+
+    val fahrenheit: Double=140.00
+    print (fahrenheit)
+    val celcius: Double=(fahrenheit-32)/1.8
+    print (celcius)
+
+    val position: Int=32
+    val row: Int=4
+    val column: Int=3
+    print (row)
+    print (column)
+
+    val degress : Double = 320.00
+    var pi : Int= 180
+    val radius : Double = (degress/pi)
+    print(radius)
+
+    val x1 : Double = 6.00
+    val y1 : Double = -3.00
+    val x2 : Double = 2.00
+    val y2 : Double = -2.00
+    val distance : Double = sqrt(pow((x2-x1),2.00)+pow((y2-y1),2.00))
+    println(distance)
+
+
+}
